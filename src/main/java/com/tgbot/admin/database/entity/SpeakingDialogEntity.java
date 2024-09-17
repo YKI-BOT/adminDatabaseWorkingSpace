f7278d41351807fed4606ca07465b092e52b65b9
Maksim Kantur <112544485+MaksimKantur@users.noreply.github.com>
2024-09-17 17:32:27 +0300

    Update SpeakingDialogEntity.java

--- src/main/java/com/tgbot/admin/database/entity/SpeakingDialogEntity.java
+++ src/main/java/com/tgbot/admin/database/entity/SpeakingDialogEntity.java
@@ -15,9 +15,13 @@ public class SpeakingDialogEntity {
     @Column(name = "full_task_text")
     String fullTaskText;
 
+    @Column(name = "user_need_to_answer")
+    String userNeedToAnswer;
+
     @Column(name = "dialog_audio_url")
     String dialogAudioUrl;
 
     @Column(name = "number_of_dialog")
     Long NumberOfDialog;
 }
+
